6d3c8299ccebea512b20757be1d0e03e45445fe5
Derrick Persson <derrickpersson@gmail.com>
2018-09-19 22:07:31 -0700

    Users endpoint working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  namespace :Api do
+    namespace :V1 do
+      resources :users, only: [:index], controller: '/api/v1/users'
+    end
+  end
 end
